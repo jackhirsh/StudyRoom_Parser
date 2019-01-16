80540a3ec13f3b116bf4ee4797bcf7963a298aed
jackhirsh <jackhirsh1998@gmail.com>
2019-01-16 09:59:00 -0500

    Updated for use in sp19

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import csv
 import datetime
 now = datetime.datetime.now()
 
+
 def PrintVertical(input_list):
-    for counter in range(0,len(input_list)):
+    for counter in range(0, len(input_list)):
         print(input_list[counter])
 
-calendar_text = open("Calendar.txt", "r")
+
+calendar_text = open("/home/hirshjack/git/StudyRoom_Parser/Calendar.txt", "r")
 read_file = calendar_text.readlines()
 
 cleaned_up = []
@@ -18,31 +20,35 @@ start_ampm = []
 end_time = []
 end_ampm = []
 room_num = []
-
-for counter in range(0,len(read_file)):
+now = datetime.datetime.now()
+for counter in range(0, len(read_file)):
     cleaned_up.append(read_file[counter].replace("\t", ""))
     cleaned_up[counter] = cleaned_up[counter].replace("\n", "")
-    cleaned_up[counter] = cleaned_up[counter].replace("Curry Student Center", "")
+    cleaned_up[counter] = cleaned_up[counter].replace(
+        "Curry Student Center", "")
     cleaned_up[counter] = cleaned_up[counter].replace("reserved", "")
     cleaned_up[counter] = cleaned_up[counter].replace("Mon", "")
     cleaned_up[counter] = cleaned_up[counter].replace("Tue", "")
     cleaned_up[counter] = cleaned_up[counter].replace("Wed", "")
     cleaned_up[counter] = cleaned_up[counter].replace("Thu", "")
     cleaned_up[counter] = cleaned_up[counter].replace("Fri", "")
-    cleaned_up[counter] = cleaned_up[counter].replace("/2018", "")
+    cleaned_up[counter] = cleaned_up[counter].replace("#", "")
+    # cleaned_up[counter] = cleaned_up[counter].replace("/2018", "")
+    cleaned_up[counter] = cleaned_up[counter].replace(
+        "/" + str(now.year), "")
 
+print("Cleaned Up\n", cleaned_up)
 
-
-for counter in range(0,len(cleaned_up)):
+for counter in range(0, len(cleaned_up)):
     current_line = cleaned_up[counter]
     month_end = current_line.find("/", 0)
     month.append(current_line[0:month_end])
-
+    print(month_end)
     day_end = current_line.find(" ", month_end)
     day.append(current_line[month_end+1:day_end])
 
     time_start = day_end+1
-    time_end = current_line.find(":",time_start)
+    time_end = current_line.find(":", time_start)
     start_time.append(current_line[time_start:time_end])
 
     ampm_start = time_end+4
@@ -57,8 +63,9 @@ for counter in range(0,len(cleaned_up)):
     ampm_end = ampm_start+2
     end_ampm.append(current_line[ampm_start:ampm_end])
 
-    num_start = ampm_end+1
+    num_start = ampm_end+2
     num_end = num_start+3
+    print("break")
     room_num.append(current_line[num_start:num_end])
 
 last_month = month[0]
@@ -67,7 +74,7 @@ last_start = str(start_time[0]) + str(start_ampm[0])
 last_end = str(end_time[0]) + str(end_ampm[0])
 print_month = True
 print_day = True
-for i in range(1,len(month)):
+for i in range(1, len(month)):
     last_month = month[i-1]
     last_day = day[i-1]
 
@@ -87,12 +94,14 @@ for i in range(1,len(month)):
     if(print_day):
         print(str(month[i]) + "/" + str(day[i]))
 
-    print("Room #" + str(room_num[i]) + " - From: " + str(cur_start) + " To " + str(cur_end))
+    print("Room #" + str(room_num[i]) + " - From: " +
+          str(cur_start) + " To " + str(cur_end))
 
-file_out = "/home/hirshjack/Calendar_Events.csv"
+file_out = "Calendar_Events.csv"
 
 with open(file_out, 'w') as csv_file:
-    fieldnames = ['Subject', 'Start Date', 'Start Time', 'End Time', 'Location']
+    fieldnames = ['Subject', 'Start Date',
+                  'Start Time', 'End Date', 'End Time', 'Location']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
@@ -100,7 +109,8 @@ with open(file_out, 'w') as csv_file:
         start_date = str(month[i]) + "/" + str(day[i]) + "/" + str(now.year)
         start = str(start_time[i]) + ":00 " + str(start_ampm[i])
         end = str(end_time[i]) + ":00 " + str(end_ampm[i])
+        end_date = start_date
         location = "Room #" + str(room_num[i]) + " Curry Student Center"
-        writer.writerow({'Subject' : "Study Room", 'Start Date' : start_date, 'Start Time' : start, 'End Time' : end, 'Location' : location})
-
-
+        writer.writerow({'Subject': "Study Room", 'Start Date': start_date,
+                         'Start Time': start, 'End Date': end_date,
+                         'End Time': end, 'Location': location})
